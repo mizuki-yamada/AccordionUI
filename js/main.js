081b60cbc35aaa6088dc18fb24c48105640efe67
mizuki-yamada <my79909369@gmail.com>
2023-05-07 10:40:19 +0900

    アニメーションを実装、1度に開く回答は1つだけになるようにappearクラスの付け外しを追加

--- js/main.js
+++ js/main.js
@@ -5,6 +5,12 @@
     dts.forEach(dt => {
         dt.addEventListener('click', () => {
             dt.parentNode.classList.toggle('appear');//dtの親ノードのみにappearをつける
+            //表示する回答を1つにするため、他のdtを調査する
+            dts.forEach(el => {
+                if (dt !== el) {
+                    el.parentNode.classList.remove('appear');
+                }
+            });
        }) 
     });
 
